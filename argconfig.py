20c50c4608962ac3f3d1cb209826649715e3d2ba
patrick-c-odriscoll <39496990+patrick-c-odriscoll@users.noreply.github.com>
2020-11-22 21:02:31 -0600

    Update argconfig.py
    Added comments to make the class easier to read.

--- argconfig.py
+++ argconfig.py
@@ -11,8 +11,15 @@ __email__ = "patrick.c.odriscoll@gmail.com"
 
 
 class argconfig(argparse.ArgumentParser):
+  """ Argconfig:
+      Improvement on the argparse.ArgumentParser class. It adds the functionality of a
+      configuration file. The order of load importance is as follows:
+        command line > config file > code set default
+      Added command line option of:
+        config=None, points to a yaml formated config file.
+  """
   def __init__(self, config=None, *args, **kargs,):
-    """ Initialization of the class """
+    ''' Initialization of the class '''
     super().__init__(*args,**kargs)
     if config is not None:
       self.config = config
@@ -24,6 +31,10 @@ class argconfig(argparse.ArgumentParser):
     return
   
   def add_argument(self, *args, **kargs):
+    ''' Add argument
+        Same as argparse.ArgumentParser with the addded functionality of:
+            Command line > config file > code set default
+    '''
     try:
       kargs['default'] = self.getConfig(args,kargs['default'])
     except:
@@ -36,6 +47,9 @@ class argconfig(argparse.ArgumentParser):
     return
 
   def getConfig(self,arg,value):
+    ''' Get configuration from config
+        Override the code based default based upon the settings in the config file. 
+    '''
     for ii in arg:
       try:
         return self.parms[ii.lstrip('-')]
@@ -45,6 +59,18 @@ class argconfig(argparse.ArgumentParser):
 
 
 if __name__ == "__main__":
+  """ Example of loader:
+    command line arugments > config file > code set defailt
+    command line argument:
+      foo : depends on user
+      bar : depends on user setting
+    example.yaml set to:
+      foo : test1
+      bar : 2.0
+    default :
+      foo : testing
+      bar : implicit None
+  """
   parser = argconfig(description='argparse + yaml config file example',config='./example.yaml')
   parser.add_argument('-f','--foo', type=str, default='testing',help='foo (default=testing, config=test)')
   parser.add_argument('--bar',                                  help='bar (default=None, config = 2.0)')
